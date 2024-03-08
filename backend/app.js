3084d91faa0428da66a84a1c7decd4b145ec6dc3
Anshul Chaturvedi <129001479+AnshulChaturvedi@users.noreply.github.com>
2024-03-08 11:15:42 +0530

    DatabaseAdded

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cors = require('cors');
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+//database connection
+const connectDB = require("./config/database");
+connectDB();
+
+//middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(cors());
+
+
+// error middleware
+app.use(errorHandler);
+
+
+//routes
+
+
+//server started
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
